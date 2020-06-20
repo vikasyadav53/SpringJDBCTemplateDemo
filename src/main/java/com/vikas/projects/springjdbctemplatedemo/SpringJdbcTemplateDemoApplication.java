4e974a79c308923fa3253b1e7e029084958e370d
Vikas Yadav <vikasyadav.a079@gmail.com>
2020-06-20 22:18:21 +0530

    Spring JDBC Template example

---
+++ src/main/java/com/vikas/projects/springjdbctemplatedemo/SpringJdbcTemplateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vikas.projects.springjdbctemplatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcTemplateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcTemplateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikas/projects/springjdbctemplatedemo/SpringJdbcTemplateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.projects.springjdbctemplatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcTemplateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
